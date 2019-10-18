30eccb0771b69c22eb4680a2044df2d5114d350d
amandapadilla <amandapadillasantiago@gmail.com>
2019-10-18 22:23:17 +0200

    prepare workspace

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../stylesheets/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="" alt="Mi lista de deseos" />
+      <h1>Lista de deseos</h1>
+      <form>
+        <input type="text" />
+        <input type="submit" value="Añadir" />
+      </form>
+      <ul>
+        <li>Info a pincho</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
