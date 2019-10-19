31d39b68488c40dcc86ca1f36935369f6674b57c
amandapadilla <amandapadillasantiago@gmail.com>
2019-10-19 14:30:54 +0200

    upgrade docs folder

---
+++ docs/precache-manifest.19ef77bebaa4a815fb51a417cbfea6d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee120e56f3c2ef6e09f44606be0bfff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "338502c3cf83a388d335",
+    "url": "./static/css/main.82d35a75.chunk.css"
+  },
+  {
+    "revision": "71ff2a8d1a7f83d483d0",
+    "url": "./static/js/2.6dc45b1b.chunk.js"
+  },
+  {
+    "revision": "338502c3cf83a388d335",
+    "url": "./static/js/main.7983e1df.chunk.js"
+  },
+  {
+    "revision": "39ace183e0234538df6b",
+    "url": "./static/js/runtime-main.9e0f61e7.js"
+  }
+]);
